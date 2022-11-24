4a7ca2d1903c3266082984a6eb444c920276dadb
Nicolas Nul <nnull31687@gmail.com>
2022-11-24 15:24:26 -0600

    Some data setup

--- SourceCode/frontend/src/store/index.js
+++ SourceCode/frontend/src/store/index.js
@@ -4,8 +4,42 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    // socket stuff
+
+    //Scorers
+    Scorers:{
+      schedule: [],
+      currentRaceId: null,
+      currentRaceRacers: [],
+      currentRaceScores: [],
+    },
+    
+
+  },
+  mutations: {
+    // Scorers
+    updateSchedule(state, {schedule}){
+      state.Scorers.schedule =schedule
+    },
+    updateCurrentRace(state,{currentRaceId,currentRaceRacers,currentRaceScores}){
+      state.Scorers.currentRaceId=currentRaceId
+      state.Scorers.currentRaceRacers=currentRaceRacers
+      state.Scorers.currentRaceScores=currentRaceScores
+    },
+  },
+  getters:{},
+  actions: {
+    //Scorers
+    async getFullSchedule(){
+      let mySchedule=[
+        {raceId:1, racers:[1,2,3,4], scores:[0,0,0,0]},
+        {raceId:2, racers:[2,3,4,1], scores:[0,0,0,0]},
+        {raceId:3, racers:[3,4,1,2], scores:[0,0,0,0]},
+        {raceId:4, racers:[4,1,2,3], scores:[0,0,0,0]}
+      ]
+      this.commit("updateSchedule", {schedule: mySchedule})
+      this.commit("updateCurrentRace", {currentRaceId: 1, racers:[1,2,3,4], scores: [0,0,0,0]})
+    }
+  },
 });
--- SourceCode/frontend/src/views/Scorers.vue
+++ SourceCode/frontend/src/views/Scorers.vue
@@ -1,19 +1,59 @@
 <template>
   <div>
-    Scoreboard <button v-on:click="toHome">Back to home</button>
+    Scorers <button v-on:click="toHome">Back to home</button>
+    <br/>
+    <v-card elevation="10">
+        hello?
+    </v-card>
+    <button v-on:click="previousRace">Previous Race</button>
+    <button v-on:click="nextRace">Next Race</button>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 import router from '../router';
 export default {
     name: "Scorers",
+    data: () =>({
+        schedule: [],
+        currentRaceId: null,
+        currentRaceRacers: [],
+        currentRaceScores: [],
+    }),
+    computed:{
+        ...mapState(["Scorers"])
+    },
+    async created(){
+        window.scrollTo(0,0)
+        await this.$store.dispatch("getFullSchedule")
+        this.schedule=this.Scorers.schedule
+        this.currentRaceId=0
+        this.currentRaceRacers=this.schedule[this.currentRaceId].racers
+        this.currentRaceScores=this.schedule[this.currentRaceId].scores
+    },
     methods:{
         toHome(){
             router.push({
                 path:"/"
             })
-        }
+        },
+        nextRace(){
+            if(this.currentRaceId < this.schedule.length-1){
+                this.currentRaceId++
+                this.currentRaceRacers=this.schedule[this.currentRaceId].racers
+                this.currentRaceScores=this.schedule[this.currentRaceId].scores
+            }
+            
+        },
+        previousRace(){
+            if(this.currentRaceId > 0){
+                this.currentRaceId--
+                this.currentRaceRacers=this.schedule[this.currentRaceId].racers
+                this.currentRaceScores=this.schedule[this.currentRaceId].scores
+            }
+            
+        },
     },
 }
 </script>
